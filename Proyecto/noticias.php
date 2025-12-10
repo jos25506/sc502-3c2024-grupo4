671fb0a25ec0aa6abd8ee9ea12e4bf2888e5d1fe
Anderson VM <andersonvm91@gmail.com>
2025-12-10 12:45:18 -0600

    cssNoticias

--- Proyecto/noticias.php
+++ Proyecto/noticias.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
-  <link rel="stylesheet" href="assets/css/blog.css">
+  <link rel="stylesheet" href="assets/css/noticias.css">
 </head>
 
 <body>
